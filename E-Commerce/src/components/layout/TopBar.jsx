87ba9730094bc232bea7931e0331032ba235cbd9
ibrahim0891 <nijhum0891@gmail.com>
2025-09-11 17:32:20 +0600

    fixed top bar and hero section design

--- E-Commerce/src/components/layout/TopBar.jsx
+++ E-Commerce/src/components/layout/TopBar.jsx
@@ -1,28 +1,26 @@
 import { faArrowDown, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CaretDownIcon } from "@phosphor-icons/react";
 import React from "react";
 
 export default function TopBar() {
   return (
     <div className="bg-green-100">
-      <div className="flex justify-between max-w-7xl m-auto p-1">
-        <p className="flex gap-2 text-green-500 text-sm items-center">
+      <div className="flex justify-between max-w-7xl m-auto py-3">
+        <p className="flex gap-2 text-hard-primary items-center justify-center">
           <FontAwesomeIcon icon={faLocationDot} />
           Store Location: Lincoln- 344, Illinois, Chicago, USA
         </p>
-        <div className="flex">
-          <button className="text-sm flex items-center gap-1 mr-4">
+        <div className="flex items-center gap-4">
+          <span className="text-sm flex items-center gap-2 mr-4">
             Eng{" "}
-            <span className="rotate-90 inline-block font-bold text-lg">
-              {">"}
-            </span>
-          </button>
-          <button className="text-sm flex items-center gap-1 mr-4">
+            <CaretDownIcon size={12}/>
+          </span>
+          <span className="text-sm flex items-center gap-2 mr-4">
             USD{" "}
-            <span className="rotate-90 inline-block font-medium text-lg">
-              {">"}
-            </span>
-          </button>
+            <CaretDownIcon size={12}/>
+          </span>
+           
         </div>
       </div>
     </div>
