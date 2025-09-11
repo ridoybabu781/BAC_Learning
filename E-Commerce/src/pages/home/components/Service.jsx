1b5279d643781fc0c8dc1fcaf663f597743e3d82
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-09-11 07:55:11 -0400

    just added some image

---
+++ E-Commerce/src/pages/home/components/Service.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Service() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={} />
+      <h3>Free Shipping</h3>
+      <p>Free shipping with discount</p>
+    </div>
+  );
+}
