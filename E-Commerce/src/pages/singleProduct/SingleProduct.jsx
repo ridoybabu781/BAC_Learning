937321ee5c4d6174c35dda6a8e3a1e88a824f1a5
mdrezuanislamridoy <itzking.rk@gmail.com>
2025-09-12 16:42:28 +0600

    commit

---
+++ E-Commerce/src/pages/singleProduct/SingleProduct.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SingleProduct() {
+  const productImages = [
+    { path: "", isSelected: false },
+    { path: "", isSelected: false },
+    { path: "", isSelected: false },
+    { path: "", isSelected: false },
+  ];
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
