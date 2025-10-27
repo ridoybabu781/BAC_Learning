7f1ad15aa315aa8f9f90bd08c1a504384066f689
MD Ridoy Babu <itzking.rk@gmail.com>
2025-10-27 11:55:45 +0600

    Delete Organic-Commerce-Backend directory

--- Organic-Commerce-Backend/src/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "your_jwt_secret_key",
-  dbUri: process.env.DB_URI || "mongodb://localhost:27017/organic-commerce",
-  port: process.env.PORT || 5500,
-};
--- Organic-Commerce-Backend/src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-require("dotenv").config();
-
-const generateToken = async (info, expiresIn = "1d") => {
-  return jwt.sign(info, process.env.JWT_SECRET, { expiresIn });
-};
-
-module.exports = generateToken;
